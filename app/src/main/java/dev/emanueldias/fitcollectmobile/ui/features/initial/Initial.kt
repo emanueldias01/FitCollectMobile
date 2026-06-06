d59033b378f952026a51367f93ac18ee6fa32204
emanueldias01 <emanueldias01dev@gmail.com>
2026-06-06 11:03:45 -0300

    implements animations

--- app/src/main/java/dev/emanueldias/fitcollectmobile/ui/features/initial/Initial.kt
+++ app/src/main/java/dev/emanueldias/fitcollectmobile/ui/features/initial/Initial.kt
@@ -1,5 +1,9 @@
 package dev.emanueldias.fitcollectmobile.ui.features.initial
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +16,11 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -24,11 +33,28 @@ import dev.emanueldias.fitcollectmobile.ui.theme.AndroidGreen
 import dev.emanueldias.fitcollectmobile.ui.theme.AndroidGreenDark
 import dev.emanueldias.fitcollectmobile.ui.theme.AndroidWhite
 import dev.emanueldias.fitcollectmobile.ui.theme.FitCollectMobileTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun InitialScreen(
     onButtonClickSearchDevices : () -> Unit
 ) {
+
+    var logoVisibility by remember{ mutableStateOf(false) }
+    var nameAppVisibility by remember { mutableStateOf(false) }
+    var textVisibility by remember { mutableStateOf(false) }
+    var buttonVisibility by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(500)
+        logoVisibility = true
+        delay(500)
+        nameAppVisibility = true
+        delay(500)
+        textVisibility = true
+        delay(500)
+        buttonVisibility = true
+    }
 
     Scaffold() { innerPadding ->
         Column(
@@ -43,47 +69,71 @@ fun InitialScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Image(
-                    painter = painterResource(R.drawable.outline_fitness_center_24),
-                    contentDescription = "logo",
-                    modifier = Modifier.size(130.dp)
-                )
+                AnimatedVisibility(
+                    visible = logoVisibility,
+                    enter = fadeIn(animationSpec = tween(600)) + slideInVertically(initialOffsetY = { it / 2 })
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.outline_fitness_center_24),
+                        contentDescription = "logo",
+                        modifier = Modifier.size(100.dp)
+                    )
 
-                Spacer(
-                    modifier = Modifier.size(20.dp)
-                )
+                    Spacer(
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
 
-                Text(
-                    text = "FitCollect",
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = AndroidGreenDark
-                )
+
+
+                AnimatedVisibility(
+                    visible = nameAppVisibility,
+                    enter = fadeIn(animationSpec = tween(600)) + slideInVertically(initialOffsetY = { it / 2 })
+                ) {
+                    Text(
+                        text = "FitCollect",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = AndroidGreenDark,
+                        modifier = Modifier.padding(30.dp)
+                    )
+                }
             }
 
-            Text(
-                "Colete seus dados através de um WearOS",
-                fontSize = 15.sp,
-                fontWeight = FontWeight.W500,
-                color = AndroidGreenDark
-            )
-
-
-            Spacer(modifier = Modifier.size(100.dp))
-
-
-            ElevatedButton(
-                onClick = onButtonClickSearchDevices,
-                colors = ButtonColors(
-                    containerColor = AndroidGreen,
-                    contentColor = AndroidWhite,
-                    disabledContainerColor = AndroidGreenDark,
-                    disabledContentColor = AndroidWhite
-                )
+            AnimatedVisibility(
+                visible = textVisibility,
+                enter = fadeIn(animationSpec = tween(600)) + slideInVertically(initialOffsetY = { it / 2 })
             ) {
+                Text(
+                    "Colete seus dados através de um WearOS",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.W500,
+                    color = AndroidGreenDark
+                )
 
-                Text("Buscar dispositivo")
+                Spacer(modifier = Modifier.size(100.dp))
+            }
 
+
+
+
+            AnimatedVisibility(
+                visible = buttonVisibility,
+                enter = fadeIn(animationSpec = tween(600)) + slideInVertically(initialOffsetY = { it / 2 })
+            ) {
+                ElevatedButton(
+                    onClick = onButtonClickSearchDevices,
+                    colors = ButtonColors(
+                        containerColor = AndroidGreen,
+                        contentColor = AndroidWhite,
+                        disabledContainerColor = AndroidGreenDark,
+                        disabledContentColor = AndroidWhite
+                    )
+                ) {
+
+                    Text("Buscar dispositivo")
+
+                }
             }
         }
     }
